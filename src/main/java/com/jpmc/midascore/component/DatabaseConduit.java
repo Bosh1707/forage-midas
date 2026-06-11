361b7a79e4c3d379b1550be37003da37a7b5e2fc
Joshua Rahul Tan Sreedharan <joshua.rahul1707@gmail.com>
2026-06-11 16:46:42 +0800

    Task Three and Task Four

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,62 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseConduit {
-    private final UserRepository userRepository;
+    private static final String INCENTIVE_URL = "http://localhost:8080/incentive";
 
-    public DatabaseConduit(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
+
+    public DatabaseConduit(UserRepository userRepository, TransactionRepository transactionRepository, RestTemplateBuilder restTemplateBuilder) {
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplateBuilder.build();
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
+    }
+
+    @Transactional
+    public void process(Transaction transaction) {
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        float amount = transaction.getAmount();
+        if (sender.getBalance() < amount) {
+            return;
+        }
+
+        float incentive = getIncentive(transaction);
+        sender.setBalance(sender.getBalance() - amount);
+        recipient.setBalance(recipient.getBalance() + amount + incentive);
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+        transactionRepository.save(new TransactionRecord(sender, recipient, amount, incentive));
+    }
+
+    private float getIncentive(Transaction transaction) {
+        ResponseEntity<Incentive> response = restTemplate.postForEntity(INCENTIVE_URL, transaction, Incentive.class);
+        Incentive incentive = response.getBody();
+        return incentive == null ? 0.0f : incentive.getAmount();
     }
 
 }
--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
 
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     // This annotation binds the method to the topic defined in your YAML file
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        // We will place our debugger breakpoint on the line below
-        float amount = transaction.getAmount();
-        System.out.println("Received transaction with amount: " + amount);
+        databaseConduit.process(transaction);
     }
 }
